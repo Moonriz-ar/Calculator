a1dbb481cee6c24a8dba9d3c7d66ca700b3b268c
Andrea <andre93.lin@gmail.com>
2021-03-11 12:22:19 -0300

    followed along web dev simplified tutorial for best practices, set up html and selected dom elements

---
+++ app.js
@@ -0,0 +1,12 @@
+class Calculator {
+    constructor()
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
